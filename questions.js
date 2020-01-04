7c6e902efccb92ea230799968596312740faf2d0
Austen Turner <austenpturner@msn.com>
2020-01-04 14:40:12 -0800

    make new file and move quiz items to new js file

---
+++ questions.js
@@ -0,0 +1,17 @@
+const quizItems = [
+    { 
+        question: 'this is a question',
+        choices: ['choice1', 'choice2', 'choice3', 'choice4'],
+        answer: 'choice1'
+    },
+    { 
+        question: 'this is another question',
+        choices: ['choice5', 'choice6', 'choice7', 'choice8'],
+        answer: 'choice8'
+    },
+    { 
+        question: 'this is a different question',
+        choices: ['choice9', 'choice10', 'choice11', 'choice12'],
+        answer: 'choice10'
+    }
+];
